a7f83498c0957fadb00363d77a3f1a0d686cb02d
aranhil <aranhil@yahoo.com>
2023-04-27 15:26:55 +0300

    work in progress

--- qgrepSearchTool/ToolWindows/SearchWindow/qgrepSearchWindow.xaml.cs
+++ qgrepSearchTool/ToolWindows/SearchWindow/qgrepSearchWindow.xaml.cs
@@ -72,6 +72,11 @@ namespace qgrepSearch.ToolWindows
         public ColorEntry[] ColorEntries = new ColorEntry[] { };
     }
 
+    public class Customer
+    {
+        public string ContentData { get; set; }
+    }
+
     public partial class qgrepSearchWindowControl : UserControl
     {
         private EnvDTE80.DTE2 DTE;
@@ -118,6 +123,15 @@ namespace qgrepSearch.ToolWindows
 
             UpdateColorsFromSettings();
             UpdateFromSettings();
+
+            ObservableCollection<Customer> custdata = new ObservableCollection<Customer>();
+            custdata.Add(new Customer() { ContentData = "content1" });
+            custdata.Add(new Customer() { ContentData = "content2" });
+            custdata.Add(new Customer() { ContentData = "content3" });
+            custdata.Add(new Customer() { ContentData = "content4" });
+            custdata.Add(new Customer() { ContentData = "content5" });
+            custdata.Add(new Customer() { ContentData = "content6" });
+            lst.ItemsSource = custdata;
         }
 
         public System.Windows.Media.Color ConvertColor(System.Drawing.Color color)
