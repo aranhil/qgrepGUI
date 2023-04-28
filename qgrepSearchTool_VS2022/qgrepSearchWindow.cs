0c0f95171b2b4be7cf0542bdd26e1ed6270a6aca
Iulian Chivu <aranhil@yahoo.com>
2023-04-29 02:39:33 +0300

    work in progress

---
+++ qgrepSearchTool_VS2022/qgrepSearchWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Imaging;
+using Microsoft.VisualStudio.Shell;
+using qgrepControls.ToolWindows;
+
+namespace qgrepSearch
+{
+    [Guid(SearchWindowGuid)]
+    public class qgrepSearchWindow : ToolWindowPane
+    {
+        public const string SearchWindowGuid = "e4e2ba26-a455-4c53-adb3-8225fb696f8b";
+        public const string Title = "qgrep Search Tool";
+
+        public qgrepSearchWindow(qgrepSearchWindowState state) : base()
+        {
+            Caption = Title;
+            BitmapImageMoniker = KnownMonikers.ImageIcon;
+            Content = new qgrepSearchWindowControl(new qgrepExtension(state));
+        }
+    }
+}
