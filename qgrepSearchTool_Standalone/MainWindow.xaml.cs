e65791b037658f552b7a7ac214e3505a2e56cff1
Iulian Chivu <aranhil@yahoo.com>
2023-04-30 04:09:20 +0300

    made a standalone version

---
+++ qgrepSearchTool_Standalone/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using qgrepControls.ToolWindows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace qgrepSearchTool_Standalone
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowContent.Children.Add(new qgrepSearchWindowControl(new qgrepExtension(this)));
+        }
+    }
+}
---
+++ qgrepSearchTool_Standalone/qgrepExtension.cs
@@ -0,0 +1,88 @@
+﻿using qgrepControls.Classes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace qgrepSearchTool_Standalone
+{
+    class qgrepExtensionWindow : IExtensionWindow
+    {
+        private Window window;
+        public qgrepExtensionWindow(Window window)
+        {
+            this.window = window;
+        }
+
+        public void ShowModal()
+        {
+            window.ShowDialog();
+        }
+
+        public void Close()
+        {
+            window.Close();
+        }
+    }
+
+    class qgrepExtension : IExtensionInterface
+    {
+        Window window = null;
+
+        public qgrepExtension(Window window)
+        {
+            window = window;
+        }
+
+        public bool WindowOpened 
+        {
+            get 
+            {
+                return false;
+            }
+            set
+            {
+            }
+        }
+
+        public IExtensionWindow CreateWindow(UserControl userControl, string title)
+        {
+            return new qgrepExtensionWindow(
+                new Window
+                {
+                    Title = title,
+                    Content = userControl,
+                    SizeToContent = SizeToContent.WidthAndHeight,
+                    ResizeMode = ResizeMode.NoResize,
+                    Owner = window
+                }
+            );
+        }
+
+        public string GetSelectedText()
+        {
+            return "";
+        }
+
+        public string GetSolutionPath()
+        {
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string appFolderPath = Path.Combine(appDataPath, "qgrepSearch");
+
+            if (!Directory.Exists(appFolderPath))
+            {
+                Directory.CreateDirectory(appFolderPath);
+            }
+
+            return appFolderPath;
+        }
+
+        public void OpenFile(string path, string line)
+        {
+        }
+    }
+}
